41e6b9a0006db045bb77cf8fc8b66f059524ee0d
rochaalex11021 <109445353+rochaalex11021@users.noreply.github.com>
2023-01-07 16:20:54 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
+import React, { Component } from "react";
+// import Particles from 'react-particles-js';
+import ParticlesBg from "particles-bg";
+import Clarifai from "clarifai";
 import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      {/*<Logo />
-      <ImageLinkForm />
-  <FaceRecognition />*/}
-    </div>
-  );
+const app = new Clarifai.App({
+  apiKey: "46b5b5e55e104cda8bbc2d39513d6eda",
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+    app.models
+      .predict(
+        "6dc7e46bc9124c5c8824be4822abe105",
+        "https://samples.clarifai.com/face-det.jpg"
+      )
+      .then(
+        function (response) {
+          console.log(response);
+        },
+        function (err) {}
+      );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg type="tadpole" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/*<FaceRecognition />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
